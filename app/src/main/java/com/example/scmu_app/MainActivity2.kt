5504b303d91d88ac39e38f4e8ac0521575fbeffc
joaosantos61045 <jpbr.santos@campus.fct.unl.pt>
2024-05-09 18:47:54 +0100

    Add System Button

--- app/src/main/java/com/example/scmu_app/MainActivity2.kt
+++ app/src/main/java/com/example/scmu_app/MainActivity2.kt
@@ -1,28 +1,41 @@
 package com.example.scmu_app
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.widget.ScrollView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.gestures.ScrollableState
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.AbsoluteRoundedCornerShape
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.ColorPainter
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.scmu_app.ui.theme.SCMUAppTheme
 
+
 class MainActivity2 : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,9 +43,12 @@ class MainActivity2 : ComponentActivity() {
         setContent {
             SCMUAppTheme {
                 Scaffold(
-                    modifier = Modifier.fillMaxSize()
-                ) { innerPadding ->
-                    AddSystemContent(Modifier.padding(innerPadding))
+
+                ) {
+
+
+                        AddSystemContent()
+
                 }
             }
         }
@@ -41,11 +57,48 @@ class MainActivity2 : ComponentActivity() {
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun AddSystemContent(modifier: Modifier = Modifier) {
+fun AddSystemContent() {
     val context = LocalContext.current
+    val timeIntervals = listOf(
+        "00:00", "00:30",
+        "01:00", "01:30",
+        "02:00", "02:30",
+        "03:00", "03:30",
+        "04:00", "04:30",
+        "05:00", "05:30",
+        "06:00", "06:30",
+        "07:00", "07:30",
+        "08:00", "08:30",
+        "09:00", "09:30",
+        "10:00", "10:30",
+        "11:00", "11:30",
+        "12:00", "12:30",
+        "13:00", "13:30",
+        "14:00", "14:30",
+        "15:00", "15:30",
+        "16:00", "16:30",
+        "17:00", "17:30",
+        "18:00", "18:30",
+        "19:00", "19:30",
+        "20:00", "20:30",
+        "21:00", "21:30",
+        "22:00", "22:30",
+        "23:00", "23:30"
+    )
+    val timeDurations = listOf(
+        "5", "10", "15", "20", "25", "30"
+    )
+    val mintGreen=Color(0xffbff4d2)
+    val darkGreen=Color(0xFF306044)
+    val bgGreen=Color(0xFF8CBF9F)
+    val swampGreen=Color(0xFF5D8E70)
+    Column(
+        modifier = Modifier.verticalScroll(rememberScrollState())
+            .background(mintGreen )
 
-    Column(modifier) {
+    ) {
         TopAppBar(
+
             title = { Text("Add System") },
             navigationIcon = {
                 IconButton(onClick = {
@@ -54,36 +107,144 @@ fun AddSystemContent(modifier: Modifier = Modifier) {
                 }) {
                     Icon(Icons.Filled.ArrowBack, contentDescription = "Go back")
                 }
-            }
+            },
         )
+
 
         Image(
             painter = painterResource(id = R.drawable.image), // Replace 'your_image_resource' with your image resource ID
             contentDescription = null, // Add appropriate content description
             modifier = Modifier
                 .fillMaxWidth()
-                .height(100.dp)
+                .height(50.dp)
         )
 
-        Subheader("General")
-        TextBox("WiFi")
+        Subheader("General",darkGreen)
+        var text =TextBox("WiFi")
         TextBox("House Name")
 
-        Subheader("Schedule")
-        Dropdown("Duration", listOf("1 hour", "2 hours", "3 hours")) // Replace with appropriate duration options
-        Dropdown("Hour", listOf("9:00 AM", "10:00 AM", "11:00 AM")) // Replace with appropriate hour options
-        Subsubheader("Rotation")
+        Subheader("Schedule",darkGreen)
+        Dropdown("Duration (in Minutes)", timeDurations,"Duration") // Replace with appropriate duration options
+        Dropdown("Hour of System Start", timeIntervals,"Hour") // Replace with appropriate hour options
+        Subheader("Rotation",darkGreen)
         // Add rotation content here
+        DaySelectionRow()
+        Subheader("Notifications",darkGreen)
+        ToggleButton("Smart Watering")
+        Subheader("Dangerous",darkGreen)
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Text(
+                text = "Remove System",
+                color = Color.Red,
+                style = MaterialTheme.typography.bodyMedium,
+                modifier = Modifier.weight(1f)
+            )
+            Button(onClick = {  },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Red
+                )) {
+                Text(text = "Remove System",
+                    color = Color.White)
+
+            }
+        }
+
+        Button(onClick = {  }) {
+            Text(text = "Add System")
+
+        }
+
+
     }
 }
 
 @Composable
-fun Subheader(text: String) {
-    Text(
+fun ToggleButton(text: String) {
+    var isChecked by remember { mutableStateOf(false) }
+
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.padding(16.dp)
+    ) {
+        Text(
+            text = text,
+            style = MaterialTheme.typography.bodyMedium,
+            modifier = Modifier.weight(1f)
+        )
+        Switch(
+            checked = isChecked,
+            onCheckedChange = { isChecked = it }
+        )
+    }
+}
+@OptIn(ExperimentalLayoutApi::class)
+@Composable
+fun DaySelectionRow() {
+    val daysOfWeek = listOf("Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun")
+    val selectedIndices = remember { mutableStateListOf<Int>() }
+
+    FlowRow(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.Absolute.Center
+    ) {
+        for (index in daysOfWeek.indices) {
+            DayButton(
+                day = daysOfWeek[index],
+               // isSelected = index in selectedIndices,
+                onToggle = { toggleDaySelection(index, selectedIndices) }
+            )
+        }
+    }
+}
+
+@Composable
+fun DayButton(
+    day: String,
+    onToggle: () -> Unit
+) {
+    var isSelected by remember { mutableStateOf(false) }
+
+    Button(
+        onClick = {
+            isSelected = !isSelected
+            onToggle()
+        },
+        modifier = Modifier.padding(2.dp),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = if (isSelected) Color.Blue else Color.LightGray
+        ),
+        shape = CircleShape
+    ) {
+        Text(
+            modifier = Modifier.padding(1.dp),
+            text = day)
+    }
+}
+private fun toggleDaySelection(index: Int, selectedIndices: MutableList<Int>) {
+    if (index in selectedIndices) {
+        selectedIndices.remove(index)
+    } else {
+        selectedIndices.add(index)
+    }
+}
+@Composable
+fun Subheader(text: String,color: Color) {
+    Box(
+        modifier = Modifier.padding( top = 15.dp, start = 0.dp, end = 16.dp)
+            .background(color, shape =AbsoluteRoundedCornerShape(0.dp,15.dp,15.dp,0.dp) ).width(200.dp)
+    ){
+        Text(
         text = text,
         style = MaterialTheme.typography.headlineMedium,
-        modifier = Modifier.padding(top = 16.dp, start = 16.dp, end = 16.dp)
-    )
+        modifier = Modifier.padding( top = 0.dp, start = 15.dp, end = 16.dp),
+            color = Color.White,
+            fontSize = 20.sp
+
+    )}
+
 }
 
 @Composable
@@ -100,22 +261,39 @@ fun TextBox(label: String) {
     )
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Dropdown(label: String, options: List<String>) {
-    var selectedIndex by remember { mutableStateOf(0) }
+fun Dropdown(label: String, options: List<String>, initialValue: String) {
+    val context = LocalContext.current
+    var expanded by remember { mutableStateOf(false) }
+    var selectedOption by remember { mutableStateOf(initialValue) }
+    Text(text = label)
+    Box(
+        modifier = Modifier
+            .wrapContentSize(Alignment.TopEnd)
+    ) {
+       ElevatedButton(
 
-    Column(modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp)) {
-        Text(label, style = MaterialTheme.typography.titleSmall)
+            onClick = { expanded = true }) {
+            Text(
+                text = selectedOption,
+
+
+                )
+        }
+
+
         DropdownMenu(
-            expanded = false,
-            onDismissRequest = { },
-            modifier = Modifier.fillMaxWidth()
+            expanded = expanded,
+            onDismissRequest = { expanded = false }
+
         ) {
-            options.forEachIndexed { index, option ->
+            options.forEach { option ->
                 DropdownMenuItem(
-                    { Text(text = option) },
+                    text = {Text(option) },
                     onClick = {
-                        selectedIndex = index
+                        selectedOption = option
+                        expanded = false
                     }
                 )
             }
